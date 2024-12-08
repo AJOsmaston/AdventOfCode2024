8a557d34b1b861a15cc549bb39db727e3301eccf
Anthony Osmaston <dajoxx@gmail.com>
2024-12-08 12:24:03 +0000

    rename tests

--- tests/test_01.py
+++ tests/test_01.py
@@ -4,16 +4,16 @@ import src.day_01 as day_01
 
 file_data = file_helper.get_line_list_from_data("1", test=True)
 
-def test_01_example():
+def test_day_01_p1_example():
     assert day_01.Historian_Hysteria(file_data) == 11
 
-def test_02_example():
+def test_day_01_p2_example():
     assert day_01.part_two(file_data) == 31
 
 actual_file_data = file_helper.get_line_list_from_data("1")
 
-def test_01_actual():
+def test_day_01_p1_actual():
     assert day_01.Historian_Hysteria(actual_file_data) == 2086478
 
-def test_02_actual():
+def test_day_01_p2_actual():
     assert day_01.part_two(actual_file_data) == 24941624
--- tests/test_08.py
+++ tests/test_08.py
@@ -4,16 +4,16 @@ from src import day_08
 file_data = file_helper.get_line_list_from_data("8", test=True)
 actual_data = file_helper.get_line_list_from_data("8")
 
-def test_08_example():
+def test_day_08_p1_example():
     assert day_08.resonant_collinearity(file_data) == 14
 
-def test_08_actual():
+def test_day_08_p1_actual():
     day_08_actual = day_08.resonant_collinearity(actual_data)
     assert  day_08_actual < 413
     assert day_08_actual == 409
 
-def test_08b_example():
+def test_day_08_p2_example():
     assert day_08.resonant_collinearity_p2(file_data) == 34
 
-def test_08b_actual():
+def test_day_08_p2_actual():
     assert day_08.resonant_collinearity_p2(actual_data) == 1308
