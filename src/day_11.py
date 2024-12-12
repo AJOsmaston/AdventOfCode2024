9ce3b40ed727f3fa559ba25fcb8e8ef668db78c4
AJOsmaston <88432025+AJOsmaston@users.noreply.github.com>
2024-12-12 08:33:41 +0000

    day 11 p2
    I should have commit after p1 - it would have shown how for part 1, I was using a fully populated queue approach. I realised after memory profiling p2 that no amount of memoization or multithreading was going to help here, so had to rethink the problem...

--- src/day_11.py
+++ src/day_11.py
@@ -1,13 +1,39 @@
-from collections import deque
+from collections import deque, Counter
 
-def plutonian_pebbles(file_text):
+def plutonian_pebbles(file_text, times):
     initial_queue = get_initial_queue(file_text)
-    
-    
+    return enact_rules_on_queue_size_with_counts(initial_queue, times)
+
 def get_initial_queue(file_text):
-    numbers = file_text.split(" ")
-    initial_queue = deque()
-    for number in numbers:
-        initial_queue.append(number)
-    return initial_queue
-    
+    numbers = map(int, file_text.split())
+    return deque(numbers)
+
+def enact_rules_on_queue_size_with_counts(initial_queue, iterations):
+    current_counts = Counter(initial_queue)
+    print(current_counts)
+    memo = {} 
+
+    for i in range(iterations):
+        next_counts = Counter()
+
+        for stone, count in current_counts.items():
+            if stone in memo:
+                contributions = memo[stone]
+            else:
+                if stone == 0:
+                    contributions = [1]
+                else:
+                    stone_length = len(str(stone))
+                    if stone_length & 1: #odd
+                        contributions = [stone * 2024]
+                    else:  #even
+                        mid = stone_length // 2
+                        stone_str = str(stone)
+                        contributions = [int(stone_str[:mid]), int(stone_str[mid:])]
+
+                memo[stone] = contributions
+            for result in contributions:
+                next_counts[result] += count
+        current_counts = next_counts
+        print(f"Iteration {i} complete: Queue size = {sum(current_counts.values())}")
+    return sum(current_counts.values())
--- tests/test_11.py
+++ tests/test_11.py
@@ -2,7 +2,7 @@ from file_helpers import file_helper
 from src import day_11
 
 file_data = file_helper.get_data("11", test=True)
-# actual_data = file_helper.get_data("11")
+actual_data = file_helper.get_data("11")
 
 def test_day_11_p1_example_6():
     assert day_11.plutonian_pebbles(file_data, times=6) == 22
@@ -10,12 +10,9 @@ def test_day_11_p1_example_6():
 def test_day_11_p1_example_25():
     assert day_11.plutonian_pebbles(file_data, times=25) == 55312
 
-# def test_day_11_p1_actual():
-#     day_08_actual = day_11.plutonian_pebbles(actual_data)
-#     assert day_08_actual == 0
+def test_day_11_p1_actual():
+    day_11_actual = day_11.plutonian_pebbles(actual_data, times=25)
+    assert day_11_actual == 200446
 
-# def test_day_11_p2_example():
-#     assert day_11.plutonian_pebbles_p2(file_data) == 0
-
-# def test_day_11_p2_actual():
-#     assert day_11.plutonian_pebbles_p2(actual_data) == 0
+def test_day_11_p2_actual():
+    assert day_11.plutonian_pebbles(actual_data, times=75) == 238317474993392
